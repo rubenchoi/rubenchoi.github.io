10611a40568fbdcf4c89070fadb98b455578af06
ruben.choi <ruben.choi@lge.com>
2021-07-20 18:44:34 +0900

    initial

---
+++ webcam/precache-manifest.93700f73fdb4f9fa46c85d8a4204577c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ce5f6fe6c3b2086fd1e95dd2b2d61f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c263d2f7c1aa5bcfb6d0",
+    "url": "./static/css/main.ccf5a4e9.chunk.css"
+  },
+  {
+    "revision": "4f692a27e6aa1f3c69eb",
+    "url": "./static/js/2.a1d9a71a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a1d9a71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c263d2f7c1aa5bcfb6d0",
+    "url": "./static/js/main.8efcf842.chunk.js"
+  },
+  {
+    "revision": "ed6ce65444d918ac56c7",
+    "url": "./static/js/runtime-main.109b2fe0.js"
+  }
+]);
